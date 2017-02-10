93c9c25616bfce3d20925f1d5ce08b4a0a465239
Vivien Siu <vivien@outlook.com>
2017-02-10 15:39:58 -0800

    Initial Commit

---
+++ TipsPls/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipsPls
+//
+//  Created by Vivien Siu on 2/10/17.
+//  Copyright © 2017 Vivien Siu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
